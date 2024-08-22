c92794dc75cf1f14b5a6a5c97c016c7218781ebf
snowshoe12 <joungbeena@gmail.com>
2024-08-22 15:59:49 +0900

    Fix: MemberDto 데이터 수신 오류 해결

--- src/main/java/com/web/demo/controller/FileApiController.java
+++ src/main/java/com/web/demo/controller/FileApiController.java
@@ -46,14 +46,17 @@ public class FileApiController {
 	@PostMapping("/upload")	
 	@ResponseBody
 	public ResponseEntity<FileUploadResultDto> upload( 
-			//MemberDto member, // 나이, 이름, 주소 정보를 통으로 받아서 전달
+			MemberDto member, // 나이, 이름, 주소 정보를 통으로 받아서 전달
 			@RequestParam("uploadFile") MultipartFile uploadFile, // 파일을 받아서 전달
 			HttpServletRequest req) {
+		// 0. 폼 데이터 확인
+		log.info(member.toString());
+		
 		// 1. 파일 저장할 경로 획득 (여기서는 톰켓내부, 실제는 클라우드의 스토리지 선택)
 		String path     = req.getServletContext().getRealPath(""); // 저장한 위치(서버측)
-		System.out.println("path:" + path);
+		log.info("path:" + path);
 		String filename = uploadFile.getOriginalFilename();	// 파일명
-		System.out.println("filename:" + filename);
+		log.info("filename:" + filename);
 		
 		// 2. 저장 -> 클라우드상의 스토리지에 저장 권장
 		try {
--- src/main/java/com/web/demo/dto/MemberDto.java
+++ src/main/java/com/web/demo/dto/MemberDto.java
@@ -1,15 +1,19 @@
 package com.web.demo.dto;
 
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 /**
  * 사용자 정보를 FormData 형식으로 통으로 보냈다
- * 사용자 정보를 통으로 받아줄 형식이 필요 => DTO로 받겠다
+ * 사용자 정보를 통으로 받아줄 형식이 필요 => DTO로 받겟다
  * 통신단에서 사용하는 DTO
  */
+@Data
+@NoArgsConstructor
 @Getter
 @Setter
 @ToString
@@ -17,13 +21,27 @@ public class MemberDto {
 	private int age;
 	private String name;
 	private String addr;
-	private String _csrf;
 	@Builder
-	public MemberDto(int age, String name, String addr, String _csrf) {
+	public MemberDto(int age, String name, String addr) {
 		super();
 		this.age = age;
 		this.name = name;
 		this.addr = addr;
-		this._csrf = _csrf;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
