e0239817b8f3e5403fe865a57c22c097b0b590ef
snowshoe12 <joungbeena@gmail.com>
2024-08-23 11:40:21 +0900

    Feat: 파일 다운로드

--- src/main/java/com/web/demo/controller/FileApiController.java
+++ src/main/java/com/web/demo/controller/FileApiController.java
@@ -10,10 +10,15 @@ package com.web.demo.controller;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +30,9 @@ import org.springframework.web.multipart.MultipartFile;
 import com.web.demo.dto.FileUploadResultDto;
 import com.web.demo.dto.MemberDto;
 import com.web.demo.service.PostService;
+import com.web.demo.service.UtilService;
 
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -35,6 +42,11 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/file")
 @Controller
 public class FileApiController {
+	@Autowired
+	private UtilService utilService;
+	@Autowired
+	private ServletContext servletContext;
+	
 	// 파일 업로드 화면 제공
 	// ~/file/upload
 	@GetMapping("/upload")
@@ -52,20 +64,24 @@ public class FileApiController {
 		// 0. 폼 데이터 확인
 		log.info(member.toString());
 		
-		// 1. 파일 저장할 경로 획득 (여기서는 톰켓내부, 실제는 클라우드의 스토리지 선택)
+		// 1. 파일 저장할 경로 획득 (여기서는 톰켓내부, 실제는 클라우드의 스토리지(fb,s3,..) 선택)
 		String path     = req.getServletContext().getRealPath(""); // 저장한 위치(서버측)
 		log.info("path:" + path);
 		String filename = uploadFile.getOriginalFilename();	// 파일명
 		log.info("filename:" + filename);
 		
 		// 2. 저장 -> 클라우드상의 스토리지에 저장 권장
+		// I/O => 반드시 예외상황이 발생될수 있다(파일io, 네트워크io, 디비io,..) => 예외처리
 		try {
+			// 원래는 원본 파일명, 변조 파일명(해싱처리, 사용자계정혹은인증정보, 시간정보 같이 섞어서 구성)
+			// 클라우드에 올리면(firebase에 업로드하면 자동 변조됨), s3는 직접 변조 필요
 			log.info(path + "/" + filename);
 			File file = new File(path + "/" + filename);
-			// 자바프로그램(판단기준) -> 하드디스크로 데이터 전송
+			// 자바프로그램(판단기준) : 업로드된파일은 메모리에 존재(객체) -> 스트림오픈 -> 하드디스크로 데이터 전송
 			BufferedOutputStream bos =  new BufferedOutputStream( new FileOutputStream(file) );
 			// 데이터가 흘러감 : MultipartFile -> 출력스트림 -> file
 			bos.write(uploadFile.getBytes());
+			//bos.flush(); 내부적으로 자동 flush에서 강제로 쏜다(대표적인 System.out.println("hi") )
 			bos.close();			
 		} catch (Exception e) {
 			log.error("파일 저장시 오류 :" + e.getMessage());
@@ -80,12 +96,47 @@ public class FileApiController {
 		FileUploadResultDto res = FileUploadResultDto.builder().code(1).message(filename).build();
 		return ResponseEntity.status(HttpStatus.OK).body( res );
 	}
-//	// 파일 다운로드 실제 처리 제공
-//	@RequestMapping("/download")
-//	public String download() {
-//		return "";
-//	}
+	// 파일 다운로드 실제 처리 제공
+	@GetMapping("/download")
+	@ResponseBody
+	public ResponseEntity<InputStreamResource> download(@RequestParam(value="filename") String filename, 
+			HttpServletRequest req) throws Exception {
+		log.info("전달내용 : " + filename);
+		// 1. 파일이 존재하는 경로 획득 
+		String path     = req.getServletContext().getRealPath("");
+		
+		// 2. 파일까지 스트림 연결		
+		try {
+			// file -> File -> FileInputStream -> InputStreamResource -> 클라이언트로 전송
+			File file = new File( path + "/" + filename);
+			InputStreamResource isr = new InputStreamResource( new FileInputStream( file ) );
+			// 3. 파일 데이터를 읽어서 응답
+			return ResponseEntity.ok()
+					.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + file.getName() )
+					.contentType(utilService.getMediaType(servletContext, filename))
+					.contentLength(file.length())
+					.body(isr);
+		} catch (Exception e) {
+			throw new Exception("파일 다운로드 오류 " + e.getMessage());
+		}
+		
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/main/java/com/web/demo/service/UtilService.java
+++ src/main/java/com/web/demo/service/UtilService.java
@@ -1,11 +1,17 @@
 package com.web.demo.service;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
+import com.web.demo.controller.FileApiController;
+
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
-//FIXME #REFACT: 유틸리티, 서비스 전반에 필요한 유틸리티, 중복 기능등을 제공하는 서비스
+//FIXME #REFACT: 유틸리티, 서비스 전반에 필요한 유틸리티, 중복기능등을 제공하는 서비스
+@Slf4j
 @Service
 public class UtilService {
 	public void setCookie(String key, String value, int expiration, HttpServletResponse response) {
@@ -18,4 +24,23 @@ public class UtilService {
 	public int toSecoundOfDay(int day) {
 		return 60*60*24*day;
 	}
+	// 미디어 타입에 대한 자동 계산, 파일명 => 미디어 타입 추출
+	public MediaType getMediaType(ServletContext servletContext, String filename) {
+		// application/json, image/png, ...
+		String mimeType = servletContext.getMimeType(filename);
+		log.info(mimeType);
+		try {
+			MediaType mediaType = MediaType.parseMediaType(mimeType);
+			return mediaType;
+		} catch (Exception e) {
+			// 마입타입 획득 오류
+			return MediaType.APPLICATION_OCTET_STREAM;
+		}
+	}
 }
+
+
+
+
+
+
